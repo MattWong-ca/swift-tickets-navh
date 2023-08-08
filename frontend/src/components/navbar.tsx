74e2cfd241b941cc3d4d5d22e45be3272407a007
MattWong-ca <matthew.yt.wong@gmail.com>
2023-08-08 00:01:48 -0400

    Update navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,11 @@
-
+import styles from './navbar.module.css';
 
 export default function Navbar() {
 	
 
 	return (
-		<div>
-			Navbar
+		<div className={styles.navbar}>
+            <img className={styles.logo} src="/images/logo.png" alt="Swift Tickets Logo"/>
 		</div>
 	);
 }
