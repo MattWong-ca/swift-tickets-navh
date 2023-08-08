a7c5c69dab56456b9a99ecc72c89465c74b1ecbb
MattWong-ca <matthew.yt.wong@gmail.com>
2023-08-08 12:21:28 -0400

    Add landing page copy + background image

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
 import type { ISuccessResult } from "@worldcoin/idkit";
 import type { VerifyReply } from "./api/verify";
 import Navbar from '../components/navbar';
+import styles from '../styles/index.module.css';
 
 export default function Home() {
 	
@@ -9,6 +10,13 @@ export default function Home() {
 	return (
 		<div>
 			<Navbar/>
+			<div className={styles.background}>
+				<div className={styles.landingcopy}>
+					<p className={styles.title}>SWIFT TICKETS</p>
+					<p className={styles.tagline}>Taylor Swift tickets belong to genuine fans.<br/>Bots will never steal your tickets again.</p>
+					<button className={styles.explorebutton} role="button">EXPLORE TICKETS</button>
+				</div>
+			</div>
 		</div>
 	);
 }
