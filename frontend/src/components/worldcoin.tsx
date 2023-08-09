eca5859a344fa6b08fa11eef2a88ce4846d98b98
MattWong-ca <matthew.yt.wong@gmail.com>
2023-08-08 23:06:36 -0400

    Update event cards + add images

--- frontend/src/components/worldcoin.tsx
+++ frontend/src/components/worldcoin.tsx
@@ -1,11 +1,15 @@
 import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
 import type { ISuccessResult } from "@worldcoin/idkit";
-import type { VerifyReply } from "./api/verify";
+import type { VerifyReply } from "../pages/api/verify";
 
 export default function Worldcoin() {
+
+	var verified = false;
+
 	const onSuccess = (result: ISuccessResult) => {
 		// This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
 		window.alert("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
+		verified = true;
 	};
 
 	const handleProof = async (result: ISuccessResult) => {
@@ -36,22 +40,22 @@ export default function Worldcoin() {
 
 	return (
 
-        <IDKitWidget
-					action={process.env.NEXT_PUBLIC_WLD_ACTION_NAME!}
-					app_id={process.env.NEXT_PUBLIC_WLD_APP_ID!}
-					onSuccess={onSuccess}
-					handleVerify={handleProof}
-					credential_types={[CredentialType.Orb, CredentialType.Phone]}
-					autoClose
-				>
-					{({ open }) =>
-						<button className="border border-black rounded-md" onClick={open}>
-							<div className="mx-3 my-1">Verify with World ID</div>
-						</button>
-					}
-				</IDKitWidget>
+		<IDKitWidget
+			action={process.env.NEXT_PUBLIC_WLD_ACTION_NAME!}
+			app_id={process.env.NEXT_PUBLIC_WLD_APP_ID!}
+			onSuccess={onSuccess}
+			handleVerify={handleProof}
+			credential_types={[CredentialType.Orb, CredentialType.Phone]}
+			autoClose
+		>
+			{({ open }) =>
+				<button /*className="border border-black rounded-md"*/ onClick={open}>
+					<div /*className="mx-3 my-1"*/>VERIFY I'M HUMAN</div>
+				</button>
+			}
+		</IDKitWidget>
 
-        /*
+		/*
 		<div>
 			<div className="flex flex-col items-center justify-center align-middle h-screen">
 				<p className="text-2xl mb-5">World ID Cloud Template</p>
@@ -71,6 +75,6 @@ export default function Worldcoin() {
 				</IDKitWidget>
 			</div>
 		</div>
-        */
+		*/
 	);
 }
--- frontend/src/pages/upcomingevents.tsx
+++ frontend/src/pages/upcomingevents.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/navbar';
 import styles from '../styles/upcomingevents.module.css';
+import WorldcoinButton from '../components/worldcoin';
 
 export default function Upcomingevents() {
 
@@ -11,8 +12,20 @@ export default function Upcomingevents() {
 
                 <div style={{ display: 'flex', marginLeft: '20px' }}>
                     <div className={styles.wcard}>
-                        <div style={{ backgroundColor: 'red', height: '100px' }}></div>
-                        <img style={{ width: '30px' }} src="/images/swiftw.webp" alt="Swift PFP 1" />
+                        {/* <div style={{ backgroundColor: 'red', height: '100px', position: 'relative' }}> */}
+                            <img style={{ backgroundColor: 'red', height: '100px', position: 'relative' }} src="/images/tscardz.jpg">
+                                {/* <img style={{ width: '100px', marginTop: '30px', marginLeft: '30px', position: 'absolute', border: '3px solid black'}} src="/images/swiftw.webp" alt="Swift PFP 1" /> */}
+                            </img>
+                            
+                        {/* </div> */}
+
+                        <p style={{ marginTop: '40px', marginLeft: '30px', fontFamily: '"Arial Narrow", Arial, sans-serif', fontWeight: 'bold', fontSize: '30px' }}>The Eras Tour - Nov 15 2024</p>
+                        <p style={{ marginLeft: '30px', fontFamily: 'Arial Mono, monospace', color: '#777777' }}>Toronto, CA | Section W</p>
+                        <div className={styles.worldcoinbutton}/*style={{ marginLeft: '30px', marginTop: '25px', fontFamily: '"Arial Narrow", Arial, sans-serif', fontWeight: '700', border: '3px solid black', borderRadius: '0' }}*/>
+                            <WorldcoinButton/>
+                        </div>
+                        
+
                     </div>
                     <div className={styles.wcard}>
                         Hi
